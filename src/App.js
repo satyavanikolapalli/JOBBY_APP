d062593033117056f52514ba50b50ced0817de92
satyavanikolapalli <88852430+satyavanikolapalli@users.noreply.github.com>
2021-12-21 12:27:35 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import {Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import Jobs from './components/Jobs'
+import NotFound from './components/NotFound'
+import JobItem from './components/JobItem'
+import Protected from './components/Protected'
 import './App.css'
 
-//These are the lists used in the application. You can move them to any component needed.
 const employmentTypesList = [
   {
     label: 'Full Time',
@@ -39,7 +45,14 @@ const salaryRangesList = [
   },
 ]
 
-// Replace your code here
-const App = () => <div>Hello World</div>
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <Protected exact path="/" component={Home} />
+    <Protected exact path="/jobs" component={Jobs} />
+    <Protected exact path="/jobs/:id" component={JobItem} />
+    <Route component={NotFound} />
+  </Switch>
+)
 
 export default App
